7b35457c910e21e9fd18b7af363d5b10480dc49a
Kunal Khelkar <132210427+kunalkhelkar123@users.noreply.github.com>
2024-07-05 15:47:52 +0530

    Update page.js

--- Ojassoft/src/app/shop/details/page.js
+++ Ojassoft/src/app/shop/details/page.js
@@ -23,7 +23,7 @@ export default function Page() {
         <MainContent />
 
         {/* RelatedProducts */}
-        <RelatedProducts />
+        // <RelatedProducts />
       </section>
 
       <Footer />
